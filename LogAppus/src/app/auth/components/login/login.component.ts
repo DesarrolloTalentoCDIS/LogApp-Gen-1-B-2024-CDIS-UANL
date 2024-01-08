09766145ebbcc5471565bc271cf82621830bf213
Diegoptb <diegoptb19@gmail.com>
2024-01-07 22:04:25 -0600

    change 21 Configurar el component Login
    Cambio significativo en el main al realizar un enlace y borrar el código previamente escrito ahí, adición de clase en styles.css y modificaciones en los archivos html y ts del login.component

--- LogAppus/src/app/auth/components/login/login.component.ts
+++ LogAppus/src/app/auth/components/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent{
+
+  formularioLog : FormGroup = this.fb.group({
+    id: ['', [Validators.required, Validators.minLength(8)]],
+    pass: ['', [Validators.required, Validators.minLength(7)]]
+  });
+
+  constructor(private fb: FormBuilder,
+    private router : Router,
+    private authService : AuthService) {}
+
+    login(){
+      console.log(this.formularioLog.value);
+      console.log(this.formularioLog.valid);
+      this.router.navigateByUrl('/dashboard');
+    }
 
 }
