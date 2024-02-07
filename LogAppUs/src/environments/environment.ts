f9329a313f53aae1f2bc2167ba01dd44befa5c4b
Desarrollo de Talento CDIS UANL <124920323+DesarrolloTalentoCDIS@users.noreply.github.com>
2024-02-07 10:13:01 -0600

    Update environment.ts

--- LogAppUs/src/environments/environment.ts
+++ LogAppUs/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://fluid-amulet-394517.wl.r.appshot.com/ap'
+  baseUrl: 'https://fluid-amulet-394517.wl.r.appshot.com/api'
 };
 
 /*
