54768df9396c4e90c79a09638622d76e03aab933
Rogel Axel <royax64@posteo.mx>
2024-02-12 04:38:14 -0600

    23 Añadir notificaciones: Feat
    Añade la librería y configura las notificaciones de la app para un mensaje básico.
    Nota: Se tuvo que importar el estilo CSS de Toastr dentro de src/styles.css en lugar de incluírlo en angular.json debido a errores de renderización.

--- LogAppUs/src/app/app.module.ts
+++ LogAppUs/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,9 @@ import { HttpClientModule } from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
